b13c84a78c7f6703a9cd7bc8e4d9f389c9b13672
julytian <chenwenyi@youcheyihou.com>
2022-10-14 10:52:03 +0800

    fix: fix theme ts warning

--- app.vue
+++ app.vue
@@ -1,14 +1,16 @@
-<script setup lang="ts">import { CNODE_USER_INFO } from './constants';
+<script setup lang="ts">
+import type { ConfigProviderTheme } from 'vant';
+import { CNODE_USER_INFO } from './constants';
 
-const theme = useTheme()
-const userInfo = useUserInfo()
+const userInfo = useUserInfo();
+const mode = computed(() => useTheme().value.value as ConfigProviderTheme);
 
 if (process.client && localStorage.getItem(CNODE_USER_INFO)) {
-  userInfo.value = JSON.parse(localStorage.getItem(CNODE_USER_INFO))
+  userInfo.value = JSON.parse(localStorage.getItem(CNODE_USER_INFO));
 }
 </script>
 <template>
-  <van-config-provider :theme="theme.value">
+  <van-config-provider :theme="mode">
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
